64ce18230c16fd993d90be360a011feadfdcc2c2
Rick Yao <abneryaoo@gmail.com>
2024-09-07 02:08:27 +0000

    feat: add compile.sh

---
+++ compile.sh
@@ -0,0 +1,5 @@
+git pull --rebase
+
+spcomp l4d2_tank_draw.sp -i /home/rick/steam/l4d2-ds/left4dead2/addons/sourcemod/scripting/include -o ./debug/tank_draw.smx
+
+cp ./debug/tank_draw.smx /home/rick/steam/l4d2-ds/left4dead2/addons/sourcemod/plugins/tank_draw.smx
