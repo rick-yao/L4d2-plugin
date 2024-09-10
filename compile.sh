42db3287efa240e7f9f6f3a231d082e37d80b3dc
Rick Yao <abneryaoo@gmail.com>
2024-09-10 09:00:27 +0800

    chore: remove unused file

--- compile.sh
+++
@@ -1,5 +0,0 @@
-git pull --rebase
-
-spcomp l4d2_tank_draw.sp -i /home/rick/steam/l4d2-ds/left4dead2/addons/sourcemod/scripting/include -o ./debug/tank_draw.smx
-
-cp ./debug/tank_draw.smx /home/rick/steam/l4d2-ds/left4dead2/addons/sourcemod/plugins/tank_draw.smx
